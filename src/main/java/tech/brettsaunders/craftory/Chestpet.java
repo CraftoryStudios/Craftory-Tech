af44e4e3ed062b9403023c7ac3a1fe69aa31127f
Brett Saunders <bjs730@student.bham.ac.uk>
2020-04-24 01:03:55 +0100

    Attempt Custom Mob MC way

---
+++ src/main/java/tech/brettsaunders/craftory/Chestpet.java
@@ -0,0 +1,124 @@
+package tech.brettsaunders.craftory;
+
+import net.minecraft.server.v1_15_R1.BlockPosition;
+import net.minecraft.server.v1_15_R1.ChatComponentText;
+import net.minecraft.server.v1_15_R1.DamageSource;
+import net.minecraft.server.v1_15_R1.EntityChicken;
+import net.minecraft.server.v1_15_R1.EntityHuman;
+import net.minecraft.server.v1_15_R1.EntityTypes;
+import net.minecraft.server.v1_15_R1.EnumHand;
+import net.minecraft.server.v1_15_R1.GenericAttributes;
+import net.minecraft.server.v1_15_R1.IBlockData;
+import net.minecraft.server.v1_15_R1.NBTTagCompound;
+import net.minecraft.server.v1_15_R1.PathfinderGoalFollowEntity;
+import net.minecraft.server.v1_15_R1.PathfinderGoalFollowParent;
+import net.minecraft.server.v1_15_R1.PathfinderGoalLookAtPlayer;
+import net.minecraft.server.v1_15_R1.PathfinderGoalMoveTowardsTarget;
+import net.minecraft.server.v1_15_R1.PathfinderGoalRandomLookaround;
+import net.minecraft.server.v1_15_R1.SoundEffect;
+import net.minecraft.server.v1_15_R1.SoundEffects;
+import net.minecraft.server.v1_15_R1.World;
+
+public class Chestpet extends EntityChicken {
+
+  private int timesClicked;
+
+  public Chestpet(EntityTypes<? extends EntityChicken> type, World world) {
+    super(EntityTypes.CHICKEN, world);
+  }
+
+  public Chestpet(World world) {
+    super(EntityTypes.CHICKEN, world);
+  }
+
+  protected void initPathfinder() {
+    //PathfinderGoalSelector#a(entity, speed, range?, ???) taken from EntityWolf
+    goalSelector.a(0,new PathfinderGoalMoveTowardsTarget(this, 2.0, 3f));
+    goal
+  }
+
+  protected void initAttributes() {
+    super.initAttributes();
+    getAttributeInstance(GenericAttributes.ARMOR).setValue(2D);
+  }
+
+  //Init stored data here (THAT IS SYNCED TO THE CLIENT, USELESS UNLESS YOU HAVE CLIENT SIDE MODS OR SOMETHING)
+  @Override
+  protected void initDatawatcher() {
+    super.initDatawatcher();
+  }
+
+  //Load NBT data
+  @Override
+  public void a(NBTTagCompound nbtTagCompound) {
+    super.a(nbtTagCompound);
+    timesClicked = nbtTagCompound.getInt("timesClicked");
+  }
+
+  //Save NBT data
+  @Override
+  public void b(NBTTagCompound nbtTagCompound) {
+    super.b(nbtTagCompound);
+    nbtTagCompound.setInt("timesClicked", timesClicked);
+  }
+
+  //Called every tick, used for things like health.
+  protected void mobTick() {
+    super.mobTick();
+  }
+
+  //Plays block sound I think?
+  protected void a(BlockPosition blockposition, IBlockData iblockdata) {
+    this.a(SoundEffects.ENTITY_CREEPER_PRIMED, 0.15F, 1.0F);
+  }
+
+  protected SoundEffect getSoundAmbient() {
+    return SoundEffects.ENTITY_CHICKEN_AMBIENT;
+  }
+
+  protected SoundEffect getSoundHurt(DamageSource damagesource) {
+    return SoundEffects.ENTITY_WOLF_HURT;
+  }
+
+  protected SoundEffect getSoundDeath() {
+    return SoundEffects.ENTITY_WOLF_DEATH;
+  }
+
+  protected float getSoundVolume() {
+    return 0.4F;
+  }
+
+  //Called every tick the entity moves, used for movement particles.
+  public void movementTick() {
+    super.movementTick();
+  }
+
+  //Called every tick, used for animations.
+  public void tick() {
+    super.tick();
+  }
+
+  //Called on death
+  public void die(DamageSource damagesource) {
+    super.die(damagesource);
+  }
+
+  //Called when damaged
+  public boolean damageEntity(DamageSource damagesource, float f) {
+    if (this.isInvulnerable(damagesource)) {
+      return false;
+    } else {
+      return super.damageEntity(damagesource, f);
+    }
+  }
+
+  //Called when right clicked, return true to cancel item actions. REMEMBER CALLED TWICE FOR BOTH HANDS
+  public boolean a(EntityHuman entityhuman, EnumHand enumhand) {
+    if(enumhand == EnumHand.MAIN_HAND) {
+      timesClicked++;
+      entityhuman.sendMessage(new ChatComponentText("You have clicked on this entity " + timesClicked + " times."));
+      return true;
+    }
+    return false;
+  }
+}
--- src/main/java/tech/brettsaunders/craftory/Craftory.java
+++ src/main/java/tech/brettsaunders/craftory/Craftory.java
@@ -1,16 +1,27 @@
 package tech.brettsaunders.craftory;
 
+import com.mojang.datafixers.DataFixUtils;
+import com.mojang.datafixers.types.Type;
 import java.io.File;
 import java.util.HashMap;
 import java.util.HashSet;
-import org.bukkit.Bukkit;
+import java.util.Map;
+import net.minecraft.server.v1_15_R1.BlockPosition;
+import net.minecraft.server.v1_15_R1.DataConverterRegistry;
+import net.minecraft.server.v1_15_R1.DataConverterTypes;
+import net.minecraft.server.v1_15_R1.Entity;
+import net.minecraft.server.v1_15_R1.EntityTypes;
+import net.minecraft.server.v1_15_R1.EnumCreatureType;
+import net.minecraft.server.v1_15_R1.IRegistry;
+import net.minecraft.server.v1_15_R1.SharedConstants;
+import net.minecraft.server.v1_15_R1.World;
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -21,6 +32,22 @@ public final class Craftory extends JavaPlugin {
   public static HashMap<Location, BeltManager> beltManagers = new HashMap<>();
   public CursedEarth cursedEarth = null;
   FileConfiguration config = getConfig();
+
+  private static EntityTypes entityTypes;
+
+  public static void registerEntity() {
+    Map<String, Type<?>> types = (Map<String, Type<?>>) DataConverterRegistry.a()
+        .getSchema(DataFixUtils.makeKey(SharedConstants.getGameVersion().getWorldVersion()))
+        .findChoiceType(DataConverterTypes.ENTITY_TREE).types();
+    types.put("minecraft:chestpet", types.get("minecraft:chicken"));
+    EntityTypes.a<Entity> a = EntityTypes.a.a(Chestpet::new, EnumCreatureType.CREATURE);
+    entityTypes = IRegistry.a(IRegistry.ENTITY_TYPE, "chestpet", a.a("chestpet"));
+  }
+
+  @Override
+  public void onLoad() {
+    registerEntity();
+  }
 
   @Override
   public void onEnable() {
@@ -60,8 +87,13 @@ public final class Craftory extends JavaPlugin {
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (command.getName().equals("matty")) {
       Player player = (Player) sender;
-      Block block = player.getLocation().add(0, 0, -2).getBlock();
-      block.setType(Material.STONE);
+      CraftWorld world = (CraftWorld) player.getLocation().getWorld();
+      World nmsWorld = world.getHandle();
+      Location location = player.getLocation();
+      Entity entity = entityTypes.createCreature(nmsWorld, null, null, null, new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ()), null, false, false);
+      nmsWorld.addEntity(entity, SpawnReason.CUSTOM);
+      player.sendMessage("Done");
+      return true;
     }
     if (command.getName().equals("setCursedSpreadRate")) {
       try {
