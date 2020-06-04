a29ffff7bdaea6799a160a8abd31e58cffcd24da
Matthew Jones <mattymjones@gmail.com>
2020-06-04 17:51:50 +0100

    spelling fix

--- src/main/java/tech/brettsaunders/craftory/utils/Blocks.java
+++ src/main/java/tech/brettsaunders/craftory/utils/Blocks.java
@@ -6,7 +6,7 @@ public class Blocks {
     public static final String GOLD_CELL = "craftory:gold_cell";
     public static final String DIAMOND_CELL = "craftory:diamond_cell";
     public static final String EMERALD_CELL = "craftory:emerald_cell";
-    public static final String SOLID_FUEL_GENERATOR = "craftory:soild_fuel_generator";
+    public static final String SOLID_FUEL_GENERATOR = "craftory:solid_fuel_generator";
     public static final String POWER_CONNECTOR = "craftory:power_connector";
     public static final String IRON_ELECTRIC_FURNACE = "craftory:iron_electric_furnace";
     public static final String GOLD_ELECTRIC_FURNACE = "craftory:gold_electric_furnace";
