066aae61f059bc3f06d55d2f200e68930ae21eae
Brett Saunders <bjs730@student.bham.ac.uk>
2020-07-17 20:32:46 +0100

    Turret Rotation, Particles

--- src/main/java/tech/brettsaunders/craftory/tech/power/core/block/machine/turret/ArrowTurret.java
+++ src/main/java/tech/brettsaunders/craftory/tech/power/core/block/machine/turret/ArrowTurret.java
@@ -1,7 +1,10 @@
 package tech.brettsaunders.craftory.tech.power.core.block.machine.turret;
 
 import java.util.HashMap;
+import java.util.Random;
+import org.bukkit.Color;
 import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
@@ -19,13 +22,13 @@ import tech.brettsaunders.craftory.persistence.Persistent;
 import tech.brettsaunders.craftory.tech.power.api.block.BaseMachine;
 import tech.brettsaunders.craftory.tech.power.api.guiComponents.GBattery;
 import tech.brettsaunders.craftory.tech.power.api.guiComponents.GTurretConfig;
-import tech.brettsaunders.craftory.utils.Logger;
 import tech.brettsaunders.craftory.utils.Pair;
 
 public class ArrowTurret extends BaseMachine {
   private static final byte C_LEVEL = 0;
   private static final byte MAX_RECEIVE = 120;
-  private ArmorStand turretHead;
+  private ArmorStand base_turret;
+  private ArmorStand gun_turret;
   private Pair<Double, Double> arcAngle;
   private TurretState turretState;
 
@@ -33,7 +36,10 @@ public class ArrowTurret extends BaseMachine {
   private Location startPoint;
   private int targetingCount;
 
+  private int counter = 0;
+
   private LivingEntity lastTargetedEntity;
+  private Random random;
 
 
   @Persistent
@@ -52,101 +58,161 @@ public class ArrowTurret extends BaseMachine {
     config.put(BlockFace.SOUTH_WEST, false);
     arcAngle = new Pair<Double, Double>((double)0,(double)0);
     turretState = TurretState.Searching;
-    startPoint = location.clone().add(0.5,0.5,0.5);
+    random = new Random();
   }
 
   public ArrowTurret() {
     super();
-    startPoint = location.clone().add(0.5,0.5,0.5);
     arcAngle = new Pair<Double, Double>((double)0,(double)0);
     turretState = TurretState.Searching;
+    random = new Random();
   }
 
   @Override
   public void beforeSaveUpdate() {
     super.beforeSaveUpdate();
-    turretHead.remove();
+    removeTurret();
   }
 
   @Override
   public void afterLoadUpdate() {
     super.afterLoadUpdate();
-    createHead();
+    createTurret();
+  }
+
+  private void removeTurret() {
+    base_turret.remove();
+    gun_turret.remove();
   }
 
   @Override
   public void blockBreak() {
-    turretHead.remove();
+    removeTurret();
   }
 
-  private void createHead() {
-    turretHead = (ArmorStand) location.getWorld().spawnEntity(location.clone().add(0.5,-0.70,0.5), EntityType.ARMOR_STAND);
-    turretHead.setArms(false);
-    turretHead.setBasePlate(false);
-    turretHead.setVisible(false);
-    turretHead.setOp(true);
-    turretHead.setInvulnerable(true);
-    turretHead.setGravity(false);
+  private void createTurret() {
+    base_turret = (ArmorStand) location.getWorld().spawnEntity(location.clone().add(0.5,-0.70,0.5), EntityType.ARMOR_STAND);
+    base_turret.setArms(false);
+    base_turret.setBasePlate(false);
+    base_turret.setVisible(false);
+    base_turret.setOp(true);
+    base_turret.setInvulnerable(true);
+    base_turret.setGravity(false);
 
-    EntityEquipment entityEquipment = turretHead.getEquipment();
-    entityEquipment.setHelmet(CustomItemManager.getCustomItem(Items.TURRET_HEAD));
+    EntityEquipment entityEquipment = base_turret.getEquipment();
+    entityEquipment.setHelmet(CustomItemManager.getCustomItem(Items.BASE_TURRET));
 
-    turretHead.setHeadPose(new EulerAngle(0,0,0));
+    base_turret.setHeadPose(new EulerAngle(0,0,0));
+
+    gun_turret = (ArmorStand) location.getWorld().spawnEntity(location.clone().add(0.5,-0.70,0.5), EntityType.ARMOR_STAND);
+    gun_turret.setArms(false);
+    gun_turret.setBasePlate(false);
+    gun_turret.setVisible(false);
+    gun_turret.setOp(true);
+    gun_turret.setInvulnerable(true);
+    gun_turret.setGravity(false);
+
+    entityEquipment = gun_turret.getEquipment();
+    entityEquipment.setHelmet(CustomItemManager.getCustomItem(Items.GUN_TURRET));
+
+    gun_turret.setHeadPose(new EulerAngle(0,0,0));
+
+    startPoint = base_turret.getEyeLocation().clone().add(0, 0.7, 0);
   }
 
   @Override
   public void updateMachine() {
+    if (arcAngle.getX() == 0 && arcAngle.getY()==0) return;
     if (turretState == TurretState.Searching) {
-      if (direction == 0) direction = 1;
-      turretHead.setHeadPose(turretHead.getHeadPose().add(0,Math.toRadians(direction), 0));
+      if (direction == 0)
+        direction = 1;
 
-      if (turretHead.getHeadPose().getY() > Math.toRadians(arcAngle.getY())) {
+      rotateTurret();
+
+      if (base_turret.getHeadPose().getY() > Math.toRadians(arcAngle.getY())) {
         direction = -1;
-      } else if (turretHead.getHeadPose().getY() < Math.toRadians(arcAngle.getX())) {
+        counter = 10;
+      } else if (base_turret.getHeadPose().getY() < Math.toRadians(arcAngle.getX())) {
         direction = 1;
       }
 
       //Ray
-      Vector vector = eulerToVector(turretHead.getHeadPose());
+      Vector vector = eulerToVector(base_turret.getHeadPose());
 
       RayTraceResult rayTraceResult = location.getWorld().rayTraceEntities(startPoint, vector, 10,entity -> !(entity instanceof ArmorStand));
       if (rayTraceResult != null && rayTraceResult.getHitEntity() != null && rayTraceResult.getHitEntity() instanceof LivingEntity) {
-        turretState = TurretState.Targeting; 
+        turretState = TurretState.Targeting;
+        counter = 0;
         lastTargetedEntity = ((LivingEntity)rayTraceResult.getHitEntity());
-        targetingCount = 200;
+        targetingCount = 100;
       }
     } else if (turretState == TurretState.Targeting) {
-      turretHead.setHeadPose(turretHead.getHeadPose().add(0,Math.toRadians(direction), 0));
+      rotateTurret();
 
-      Vector vector = eulerToVector(turretHead.getHeadPose());
+      Vector vector = eulerToVector(base_turret.getHeadPose());
       RayTraceResult rayTraceResult = location.getWorld().rayTraceEntities(startPoint, vector, 10,entity -> !(entity instanceof ArmorStand));
       if (rayTraceResult != null && rayTraceResult.getHitEntity() != null && rayTraceResult.getHitEntity() instanceof LivingEntity) {
         lastTargetedEntity = ((LivingEntity)rayTraceResult.getHitEntity());
-        lastTargetedEntity.damage(2);
-        targetingCount = 200;
+        onShoot(vector, rayTraceResult.getHitPosition());
+        targetingCount = 100;
         direction = 0;
       } else {
+        counter = 0;
         targetingCount--;
-        //Vector toTarget = lastTargetedEntity.getLocation().toVector().subtract(location.toVector());
-        double dx = location.getX() - lastTargetedEntity.getLocation().getX();
-        double dz = location.getZ() - lastTargetedEntity.getLocation().getZ();
-        double angle = dx*vector.getX() + dz*vector.getZ();
-        //double angle = Math.toDegrees(vector.dot(lastTargetedEntity.getLocation().toVector()));
+        double angle = (Math.atan2(location.getX() - lastTargetedEntity.getLocation().getX(), location.getZ() - lastTargetedEntity.getLocation().getZ()));
+        angle = (-(angle / Math.PI) * 360.0d) / 2.0d + 180.0d;
+        angle = (angle - Math.toDegrees(base_turret.getHeadPose().getY()));
 
-        direction = angle > 0 ? 2 : -2;
-        Logger.info(angle+"");
+        if (angle > 0.5) {
+          direction = 2;
+        } else if (angle < -0.5){
+          direction = -2;
+        } else {
+          direction = 0;
+        }
       }
 
-      if (targetingCount <= 0) turretState = TurretState.Searching;
-      if (lastTargetedEntity.getHealth() <=0) turretState = TurretState.Searching;
+      if (targetingCount <= 0 || lastTargetedEntity.getHealth() <=0) {
+        turretState = TurretState.Searching;
+      }
 
-      if (turretHead.getHeadPose().getY() > Math.toRadians(arcAngle.getY())) {
+      if (base_turret.getHeadPose().getY() > Math.toRadians(arcAngle.getY())) {
         direction = 0;
-      } else if (turretHead.getHeadPose().getY() < Math.toRadians(arcAngle.getX())) {
+      } else if (base_turret.getHeadPose().getY() < Math.toRadians(arcAngle.getX())) {
         direction = 0;
       }
     }
 
+  }
+
+  private void onShoot(Vector turretVector, Vector hitPoint) {
+    if (counter % 5 == 0) {
+      lastTargetedEntity.damage(5);
+      if (counter == 0) {
+        Location particlePoint = startPoint.clone().add(turretVector.clone().multiply(1.2));
+        Vector turretDirectionVector = turretVector.clone();
+        turretDirectionVector.multiply(0.2);
+        double addedLength = turretDirectionVector.length();
+        double distance = particlePoint.toVector().distance(hitPoint);
+        for (double length = 0; length < distance; particlePoint.add(turretDirectionVector)) {
+          location.getWorld()
+              .spawnParticle(Particle.REDSTONE, particlePoint, 1, new Particle.DustOptions(
+                  Color.GRAY, (1 + random.nextFloat() * 0.5f)));
+          length += addedLength;
+        }
+      }
+      counter++;
+    } else if (counter >= 10) {
+      counter = 0;
+    } else {
+      counter++;
+    }
+  }
+
+  private void rotateTurret() {
+    EulerAngle turnAngle = base_turret.getHeadPose().add(0, Math.toRadians(direction), 0);
+    base_turret.setHeadPose(turnAngle);
+    gun_turret.setHeadPose(turnAngle);
   }
 
   @Override
