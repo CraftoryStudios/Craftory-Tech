cab703ad0e4a733b461fba1990ab4b1db2f0a8a1
Brett Saunders <bjs730@student.bham.ac.uk>
2020-07-22 17:44:00 +0100

    CopyRight Update

--- src/main/java/tech/brettsaunders/craftory/api/blocks/CustomBlockData.java
+++ src/main/java/tech/brettsaunders/craftory/api/blocks/CustomBlockData.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2020. BrettSaunders & Craftory Team - All Rights Reserved
+ *
+ * This file is part of Craftory.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential
+ *
+ * File Author: Brett Saunders
+ ******************************************************************************/
+
 package tech.brettsaunders.craftory.api.blocks;
 
 public class CustomBlockData {
