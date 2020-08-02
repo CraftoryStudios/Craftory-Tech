442b934eef3c79367843b2a189f29e111ce637b3
Matthew Jones <mattymjones@gmail.com>
2020-08-02 19:46:41 +0100

    Removed Item slot from solar panels GUI

--- src/main/java/tech/brettsaunders/craftory/tech/power/api/block/BaseSolarGenerator.java
+++ src/main/java/tech/brettsaunders/craftory/tech/power/api/block/BaseSolarGenerator.java
@@ -13,7 +13,12 @@ package tech.brettsaunders.craftory.tech.power.api.block;
 
 import org.bukkit.Location;
 import org.bukkit.World.Environment;
+import org.bukkit.inventory.Inventory;
 import tech.brettsaunders.craftory.Utilities;
+import tech.brettsaunders.craftory.api.font.Font;
+import tech.brettsaunders.craftory.tech.power.api.guiComponents.GBattery;
+import tech.brettsaunders.craftory.tech.power.api.guiComponents.GIndicator;
+import tech.brettsaunders.craftory.tech.power.api.guiComponents.GOutputConfig;
 
 public class BaseSolarGenerator extends BaseGenerator{
 
@@ -51,6 +56,15 @@ public class BaseSolarGenerator extends BaseGenerator{
     double diff = Math.abs(time-6000)/1000d; //might be too complex
     int amount =  ((int) Math.round((-0.555*(diff*diff))))+ NOON_OUTPUT; //same for this
     return amount;
+  }
+
+  @Override
+  public void setupGUI() {
+    Inventory inventory = createInterfaceInventory(displayName, Font.BLANK.label + "");
+    addGUIComponent(new GBattery(inventory, energyStorage));
+    addGUIComponent(new GIndicator(inventory, runningContainer, 31));
+    addGUIComponent(new GOutputConfig(inventory, sidesConfig, true));
+    this.inventoryInterface = inventory;
   }
 
 }
