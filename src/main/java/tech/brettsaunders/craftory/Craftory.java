d83cddf93959510d72285106eb3ddbcfe5c0f997
Brett Saunders <brett_saunders@btinternet.com>
2020-09-25 20:03:58 +0100

    Updated Resource Pack

--- src/main/java/tech/brettsaunders/craftory/Craftory.java
+++ src/main/java/tech/brettsaunders/craftory/Craftory.java
@@ -55,8 +55,8 @@ import tech.brettsaunders.craftory.world.WorldGenHandler;
 public final class Craftory extends JavaPlugin implements Listener {
 
   public static final int SPIGOT_ID = 81151;
-  public static final String RESOURCE_PACK = "https://download.mc-packs.net/pack/1ce9a441835a541ca7c691bf1a07e9728054e984.zip";
-  public static final String HASH = "1ce9a441835a541ca7c691bf1a07e9728054e984";
+  public static final String RESOURCE_PACK = "https://download.mc-packs.net/pack/f539edb7419b4aa0a4a3d4c29e2a9e23a6f82d4a.zip";
+  public static final String HASH = "f539edb7419b4aa0a4a3d4c29e2a9e23a6f82d4a";
   public static String VERSION;
   public static PowerConnectorManager powerConnectorManager;
   public static CustomBlockFactory customBlockFactory;
