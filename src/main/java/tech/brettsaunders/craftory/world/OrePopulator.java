ce4729d77b0b96e9a5dfcdd33f542f8431ec2bcd
Brett Saunders <brett_saunders@btinternet.com>
2021-07-05 17:03:23 +0100

    fix: disable old copper ore spawning

--- src/main/java/tech/brettsaunders/craftory/world/OrePopulator.java
+++ src/main/java/tech/brettsaunders/craftory/world/OrePopulator.java
@@ -18,14 +18,13 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.generator.BlockPopulator;
-import tech.brettsaunders.craftory.Constants.Blocks;
 import tech.brettsaunders.craftory.Craftory;
 
 public class OrePopulator extends BlockPopulator {
 
   @Override
   public void populate(World world, Random random, Chunk chunk) {
-    spawnOre(random, chunk, Blocks.COPPER_ORE, 8, 60, 3, 6, 10, 60);
+    //spawnOre(random, chunk, Blocks.COPPER_ORE, 8, 60, 3, 6, 10, 60);
   }
 
   private void spawnCommonOre(Random r, Chunk chunk, String ore) {
