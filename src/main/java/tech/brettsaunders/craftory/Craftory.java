d40f610112967404a4e14e7cc6e0e64fee103e11
Mateusz Łach <mateoboss@gmail.com>
2023-03-21 23:04:44 +0100

    [core] 1.19.3 support

--- src/main/java/tech/brettsaunders/craftory/Craftory.java
+++ src/main/java/tech/brettsaunders/craftory/Craftory.java
@@ -75,7 +75,7 @@ public final class Craftory extends JavaPlugin implements Listener {
   private SentryClient sentryClient;
   private static HashSet<String> loadedPlugins = new HashSet<>();
 
-  public static final Version MAX_SUPPORTED_MC = new Version("1.19.0");
+  public static final Version MAX_SUPPORTED_MC = new Version("1.19.3");
   public static final Version MIN_SUPPORTED_MC = new Version("1.15.1");
   public static boolean isCaveAndCliffsUpdate;
   public static Version mcVersion;
